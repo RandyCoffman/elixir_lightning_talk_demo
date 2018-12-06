eea7bde6d21f7c8377cb4542943e04ea969cdf15
unknown <randy.coffman@wvjc.edu>
2018-12-05 21:08:26 -0500

    added function to quickly print numbers just on this. Added in comments to help if someone wanted to view the code after the talk in their spare time.

--- fizzbuzz/lib/fizzbuzz.ex
+++ fizzbuzz/lib/fizzbuzz.ex
@@ -1,8 +1,13 @@
 defmodule Fizzbuzz do
 
-  def try(num) when rem(num,  15) === 0, do: "FizzBuzz"
-  def try(num) when rem(num,  5) === 0, do: "Buzz"
-  def try(num) when rem(num,  3) === 0, do: "Fizz"
-  def try(num), do: num 
+  def try(num) when rem(num,  15) === 0, do: IO.puts "FizzBuzz" # Does num % 15 === 0 then returns FizzBuzz
+  def try(num) when rem(num,  5) === 0, do: IO.puts "Buzz" # Does num % 5 === 0 then returns Buzz
+  def try(num) when rem(num,  3) === 0, do: IO.puts "Fizz" # Does num % 3 === 0 then returns Fizz
+  def try(num), do: num  # Just returns num since it didn't hit the earlier when statements
 
+  def printNums(min, max) do
+    for num <- min..max, do: IO.puts Fizzbuzz.try(num) #loops through min max to print all the Fizzbuzz numbers between
+  end
 end
+
+Fizzbuzz.printNums(1, 100)
